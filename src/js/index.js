4658bd08f29bee5455e98569a37918e3f47621b9
jonamin <jounanmin@hotmail.com>
2019-08-27 18:25:58 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,7 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+  let top = ["&spade", "&heart", "&club", "&diamond"];
+  let middle = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let bottom = ["&spade", "&heart", "&club", "&diamond"];
 };
