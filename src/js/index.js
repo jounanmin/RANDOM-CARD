9525600d1085c3e64657e21113421afd502fcb7d
jonamin <jounanmin@hotmail.com>
2019-08-30 15:44:46 +0000

    added the math.random feature

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,13 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
-  let top = ["&spade", "&heart", "&club", "&diamond"];
+  let top = ["&#9824", "&#9827", "&#9829", "&#9830"];
   let middle = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-  let bottom = ["&spade", "&heart", "&club", "&diamond"];
+  let randomNumber = middle[Math.floor(Math.random() * middle.length)];
+  let randomSuite = top[Math.floor(Math.random() * top.length)];
+  console.log(randomSuite);
+
+  document.querySelector("#top").innerHTML = randomSuite;
+  document.querySelector("#middle").innerHTML = randomNumber;
+  document.querySelector("#bottom").innerHTML = randomSuite;
 };
